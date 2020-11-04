4e1750a5541e15928cc4994847e4c7e1aed2a65d
Ruslan Lizogub <rlizogub@kaaiot.io>
2020-11-04 10:36:44 +0200

    Cucumber HW

--- features/step_definitions/userCRUD_SD.js
+++ features/step_definitions/userCRUD_SD.js
@@ -7,15 +7,22 @@ const expect = require('chai').expect;
 
 const user = userData.getUser();
 const updateUser = userData.getUser({ name: "morpheus updated"});
-let userId, res;
 
 When('user create', async function () {
-    res = await userHelper.createNewUser(agent, user);
-    console.log("QQQQ ", res.status);
-    userId = res.body.id;
+    this.res = await userHelper.createNewUser(agent, user);
+    this.userId = this.res.body.id;
 });
 
-Then('check that user was created', function (expectedAnswer) {
-    expect(res.status).to.equal(201);
-    expect(res.body.name).to.equal(user.name);
+Then('check that user was created', function () {
+    expect(this.res.status).to.equal(201);
+    expect(this.res.body.name).to.equal(user.name);
+});
+
+When('user updated', async function () {
+    this.res = await userHelper.updateUser(agent, updateUser, this.userId);
+});
+
+Then('check that user was updated', function () {
+    expect(this.res.statusCode).to.equal(200);
+    expect(this.res.body.name).to.equal(updateUser.name);
 });
